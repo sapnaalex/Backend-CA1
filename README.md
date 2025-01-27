d41bcf9edae19b006a5508898a5e020a8b3c2944
unknown <sapna.alex03@gmail.com>
2025-01-27 09:23:12 +0530

    commiteed the changes in server.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Backend-CA1
+
+Deployment Link:  
---
+++ server.js
@@ -0,0 +1,29 @@
+const {express } = require('express')
+import { express } from express
+// const app = express();
+const port = 3000
+
+
+
+app.get("/signup", (req, res)=>{
+    res.send("Signup Page");
+
+    let username = Object.input; 
+    let email = Object.input;
+    let date = Object.date.Day.input;
+    let password = Object.input;
+
+    if (username==NOTFOUND){
+        console.log("Username cannot be empty");
+    }
+    
+    if (email==NOTFOUND){
+        console.log("Email cannot be empty");
+    }
+
+    if(password.length()<8 || password.length()>16){
+        console.log("Password lenght should be greater than 8 or less than or equal to 16")
+    }
+})
+
+app.listen(port, () => console.log(`App listening on port ${port}!`)) = require("dns");
